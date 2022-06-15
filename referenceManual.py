f471e00788677fe1aa88858924a52dd07c0e37ff
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-06-15 08:25:14 +0530

    Update referenceManual.py

--- referenceManual.py
+++ referenceManual.py
@@ -5,5 +5,5 @@ def colorcode_pair(MAJOR_COLORS,MINOR_COLORS):
          print("---------------------------------------------------------")
          for major_color in MAJOR_COLORS:
             for minor_color in MINOR_COLORS:
-               print ("   {:<21} {:<21} {:<21}".format(pairNumber,major_color, minor_color))
+               print ("    {:<20} {:<21} {:<21}".format(pairNumber,major_color, minor_color))
                pairNumber=pairNumber+1
