03f83c930d8cd0c5da88592a045bfbd2bc280888
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-06-15 08:37:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from referenceManual import colorcode_pair
 from colorCodePairUnitTest import *
 
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+MINOR_COLORS = ['Blue', 'Orange', 'Green', 'Brown', 'Slate']
 
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
